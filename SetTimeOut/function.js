5eb3e211b758e97787d52984c2a53f82c7916a53
arinja <sorokhaibanarinja@gmail.com>
2026-02-27 12:21:35 +0530

    newwwish

---
+++ SetTimeOut/function.js
@@ -0,0 +1,35 @@
+function sayhello(){
+    console.log("hello");
+}
+sayhello();
+
+
+function add(a,b)
+{
+    return a+b;
+}
+console.log(add(2,4));
+console.log(add('a','b'));
+
+function product(a,b)
+{
+    return a*b;
+}
+
+let product1=(a,b)=>a*b;
+
+console.log(product1(2,6));
+console.log(product1(1,1));
+console.log(product1(2,2));
+
+
+function a(d)
+{
+    d();
+    console.log("inside a");
+}
+function b()
+{
+    console.log("inside b");
+}
+a(b);
---
+++ SetTimeOut/setTimeout27feb.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log("rem after 2s");
+}, 2000);
+
+console.log("one");
+setTimeout(() => {
+    console.log("one.one");
+}, 2000);
+
+console.log("troooo");
+setTimeout(() => {
+    console.log("one,troooo");
+}, 0);
